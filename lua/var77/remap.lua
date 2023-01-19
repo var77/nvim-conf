1da307eb40829d174a87824a267e0add53ee15fb
Varik Matevosyan <varikmatevosyan@gmail.com>
2023-01-19 14:11:08 +0400

    update remaps

--- lua/var77/remap.lua
+++ lua/var77/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", ":wq<CR>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q!<CR>")
+
+vim.keymap.set({ "n", "v" }, "<Up>", "<Nop>")
+vim.keymap.set({ "n", "v" }, "<Down>", "<Nop>")
+vim.keymap.set({ "n", "v" }, "<Left>", "<Nop>")
+vim.keymap.set({ "n", "v" }, "<Right>", "<Nop>")
